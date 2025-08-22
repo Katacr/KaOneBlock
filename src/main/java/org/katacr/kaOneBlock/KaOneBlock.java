8b3be29a00204a62912acf49bc10ac76ecff9c4d
Katacr <614401508@qq.com>
2025-08-22 22:29:35 +0800

    Add files via upload

---
+++ src/main/java/org/katacr/kaOneBlock/KaOneBlock.java
@@ -0,0 +1,17 @@
+package org.katacr.kaOneBlock;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KaOneBlock extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
